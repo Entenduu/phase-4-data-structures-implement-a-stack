dcef74f9040e6d11ce7a94ee7a3c5ffb18341b92
Cristian <Entenduu@gmail.com>
2022-09-14 12:06:00 -0400

    commit tree aa25a382964d615170e0e10ab9d887c3817e6730

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,22 @@
-# your code here
+class Stack
+    def initialize
+        @stack = []
+    end
+
+    def push(value)
+        stack.push(value)
+    end
+
+    def pop
+        removed = stack.pop
+        removed
+    end
+
+    def peek
+        last = stack.length - 1
+        stack[last]
+    end
+
+    private
+    attr_reader :stack
+end
